d9cfffdc1cccd4a88cf44e1b4f925453955b0979
TheNuggitMan <64538216+TheNuggitMan@users.noreply.github.com>
2020-06-19 18:29:58 +0100

    new folio site

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById("onHover").addEventListener("mouseover", mouseOver);
+document.getElementById("onHover").addEventListener("mouseout", mouseOut);
+
+function mouseOver () {
+  setTimeout(() => document.getElementById("name").style.color = "white", 200);
+  console.log("check")
+  }
+
+  function mouseOut() {
+    document.getElementById("name").style.color = "firebrick";
+  }
