e440c0bb378a4ae78bbd9aa163f70734a3d02e7a
TheNuggitMan <thenuggitman@gmail.com>
2020-10-08 17:29:47 +0100

    Seriously Sorted the responsiveness out!

--- js/master.js
+++ js/master.js
@@ -3,14 +3,32 @@ document.getElementById("onHover").addEventListener("mouseout", mouseOut);
 
 function mouseOver () {
   setTimeout(() => document.getElementById("name").style.color = "white", 200);
-  console.log("check");
 }
 
 function mouseOut() {
   document.getElementById("name").style.color = "firebrick";
 }
 
-
-function softScroll(){
-document.getElementById("about").scrollIntoView({behavior:'smooth'});
+function softScroll2(pressed) {
+switch (pressed) {
+  case about:
+    document.getElementById("about").scrollIntoView({behavior:'smooth'})
+    console.log('success');
+    break;
+  case projects:
+    document.getElementById("projects").scrollIntoView({behavior:'smooth'})
+    console.log('success p');
+    break;
+  case contact:
+    document.getElementById("contact").scrollIntoView({behavior:'smooth'})
+    console.log('success c');
+    break;
+  default:
+    console.log('none pressed');
 }
+};
+
+function scrollUp() {
+  console.log('tada?');
+  document.querySelector("html").scrollIntoView({behavior:'smooth'});
+};
