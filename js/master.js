93b53f27ac7903afe868b976016e3bc0642087ac
TheNuggitMan <thenuggitman@gmail.com>
2020-10-09 15:46:15 +0100

    Adding Switch

--- js/master.js
+++ js/master.js
@@ -1,5 +1,7 @@
-document.getElementById("onHover").addEventListener("mouseover", mouseOver);
-document.getElementById("onHover").addEventListener("mouseout", mouseOut);
+const hovering = document.getElementById("onHover").addEventListener("mouseover", mouseOver);
+const hoverOut = document.getElementById("onHover").addEventListener("mouseout", mouseOut);
+
+
 
 function mouseOver () {
   setTimeout(() => document.getElementById("name").style.color = "white", 200);
@@ -9,26 +11,25 @@ function mouseOut() {
   document.getElementById("name").style.color = "firebrick";
 }
 
-function softScroll2(pressed) {
+function softScroll(pressed) {
 switch (pressed) {
   case about:
     document.getElementById("about").scrollIntoView({behavior:'smooth'})
-    console.log('success');
+    console.log('about');
     break;
   case projects:
     document.getElementById("projects").scrollIntoView({behavior:'smooth'})
-    console.log('success p');
+    console.log('projects');
     break;
   case contact:
     document.getElementById("contact").scrollIntoView({behavior:'smooth'})
-    console.log('success c');
+    console.log('contact');
+    break;
+  case top:
+    document.querySelector("html").scrollIntoView({behavior:'smooth'});
+    console.log('top');
     break;
   default:
     console.log('none pressed');
 }
-};
-
-function scrollUp() {
-  console.log('tada?');
-  document.querySelector("html").scrollIntoView({behavior:'smooth'});
 };
